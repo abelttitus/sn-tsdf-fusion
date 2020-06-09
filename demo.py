950db5a00bbff0dc54e00a659814c1525291747b
abelttitus <abelttitus1998@gmail.com>
2020-06-09 19:13:18 +0530

    0

--- demo.py
+++ demo.py
@@ -44,9 +44,12 @@ if __name__ == "__main__":
     #depth_im[depth_im == 65.535] = 0  # set invalid depth to 0 (specific to 7-scenes dataset)
     #cam_pose = np.loadtxt("data/frame-%06d.pose.txt"%(i))  # 4x4 rigid transformation matrix
     cam_pose=cam_poses[4*i:4*(i+1),:]
+    print "Cam Pose shape",cam_pose.shape
+    
     #print "Concatenated Cam pose",cam_pose
     # Compute camera view frustum and extend convex hull
     view_frust_pts = fusion.get_view_frustum(depth_im, cam_intr, cam_pose)
+    print "View Frust points shape",view_frust_pts.shape
     vol_bnds[:,0] = np.minimum(vol_bnds[:,0], np.amin(view_frust_pts, axis=1))
     vol_bnds[:,1] = np.maximum(vol_bnds[:,1], np.amax(view_frust_pts, axis=1))
     i+=1
